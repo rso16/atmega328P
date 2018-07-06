55b00f1990e6f2ac515231c6e5484fe8dfb438e0
rso16 <rick.overhorst@gmail.com>
2018-07-06 16:54:31 +0200

    added comments

--- Atmega328P.cpp
+++ Atmega328P.cpp
@@ -1,7 +1,102 @@
 #include "Atmega328P.h"
-//made by Rick Overhorst
-//varius handy dandy fuction for the atmel atmega 328 chip/Arduino
+//Made by Rick Overhorst
 
+
+//UART
+//Function to initialize the UART, takes the amount of data bits (5,6,6,8 or 9 bits), no, even or an odd parity bit, the amound of stop bits (1 or 2), baudrate and whether or not receive and transmit should be enabled
+void Atmega328P::UARTInit(uint8_t dataBits, uint8_t parityBit, uint8_t stopBits, float baud, uint8_t speed, uint8_t RT)
+{
+  uint16_t ubrr = round(((clockspeed / ((speed + 1) * 8 * baud)) - 1));
+  UCSR0A &= ~(1 << U2X0);
+  UBRR0H = (unsigned char)(ubrr>>8);
+  UBRR0L = (unsigned char)ubrr;
+  // Enable receiver and transmitter
+  UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<UCSZ02);
+  /* Set frame format: 8data, 1 stop bit */
+  UCSR0C = (3<<UCSZ00);
+}
+
+//Function to begin the UART with the defaul settings and a baudrate.
+void Atmega328P::UARTBegin(long int baud)
+{
+  UARTInit(0, 0, 0, baud, NORMALSPEED, 0);
+}
+
+//Function to send 9 bits, warning: not yet implemented
+void Atmega328P::UARTSend(uint16_t data)
+{
+  //not yet implemented
+}
+
+//function to send less 9 bits
+void Atmega328P::UARTSend(uint8_t data)
+{
+  //From the Atmega328P datasheet
+  /* Wait for empty transmit buffer */
+  while ( !( UCSR0A & (1<<UDRE0)) );
+  /* Put data into buffer, sends the data */
+  UDR0 = data;
+ }
+
+//Function to send multiple bytes. Takes an Array of bytes to send and the amount of bytes to send
+void Atmega328P::UARTSendBytes(uint8_t bytes[], uint8_t amountOfBytes)
+{
+  int index = 0;
+  while(index < amountOfBytes - 1)
+  {
+    UARTSend(bytes[index]);
+    index++;
+  }
+}
+
+//Function to read a byte
+uint8_t Atmega328P::UARTRead()
+{
+  int counter = 0;
+  while ((!(UCSR0A & _BV(RXC0))) && counter < 75)
+  {
+    _delay_ms(1);
+    counter++;
+  }
+  return (uint8_t) UDR0;
+}
+
+//Function to read multiple bytes. Takes an Array of bytes/pointer to put the bytes in when they are read and the amount of bytes to read
+void Atmega328P::UARTREADBytes(uint8_t *bytes, uint8_t amountOfBytes)
+{
+  int index = 0;
+  uint8_t* temp = bytes;
+  temp = bytes + (sizeof(uint8_t));
+    while(index <= amountOfBytes)
+    {
+      temp = bytes + (sizeof(uint8_t) * index);
+      *temp =  UARTRead();
+      index++;
+    }
+}
+
+//Function to sends a char array/string and sets  a new line
+void Atmega328P::print(uint8_t *bytes)
+{
+  int index = 0;
+  while (bytes[index] != '\0')
+  {
+    index++;
+  }
+  UARTSendBytes(bytes, index + 1 );
+}
+
+//Function to sends a char array/string and doesn't set a new line
+void Atmega328P::println(uint8_t *bytes)
+{
+  print(bytes);
+  UARTSend((uint8_t) 0x0A);
+  UARTSend((uint8_t) 0x0D);
+}
+
+//GPIO
+
+//Stands for set Digital Pin Mode. Sets the data directions of a digital pin (pinnumbers are based on the arduino uno board) value = 0 or 1 for input or output respectively
 void Atmega328P::setDPM(uint8_t pin, uint8_t value)
 {
   if(value==0)
@@ -28,6 +123,7 @@ void Atmega328P::setDPM(uint8_t pin, uint8_t value)
   }
 }
 
+//Stands for set AnaLog Pin Mode. Sets the data directions of a analog pin (pinnumbers are based on the arduino uno board) value = 0 or 1 for input or output respectively
 void Atmega328P::setAPM(uint8_t pin, uint8_t value)
 {
   if(value==0)
@@ -40,6 +136,7 @@ void Atmega328P::setAPM(uint8_t pin, uint8_t value)
   }
 }
 
+//Stands for Digital Write(dw). Sets the pin (based on the pinnumbers on the arduino uno board) to a logical LOW or HIGH based on value which can be 0 or 1.
 void Atmega328P::DW(uint8_t pin, uint8_t value)
 {
   if(value==0)
@@ -66,6 +163,7 @@ void Atmega328P::DW(uint8_t pin, uint8_t value)
   }
 }
 
+//Stands for Digital Read, if the pin has been set to input this function can read if it's set to a logical LOW or HIGH
 uint8_t Atmega328P::DR(uint8_t pin)
 {
     uint8_t bit=0B0;
@@ -80,6 +178,7 @@ uint8_t Atmega328P::DR(uint8_t pin)
     return bit;
 }
 
+//This function is not PWM, this function mimics the DW (Digital Write) but with the analog pins
 void Atmega328P::AW(uint8_t pin, uint8_t value)
 {
   if(value==0)
@@ -92,6 +191,7 @@ void Atmega328P::AW(uint8_t pin, uint8_t value)
   }
 }
 
+//Stands for Analog Read. Function to the read an analog value value on the pin (based on the Arduino UNO pinnumbers)
 uint16_t Atmega328P::AR(uint8_t pin)
 {
   ADMUX &= 0xf0;
@@ -104,12 +204,14 @@ uint16_t Atmega328P::AR(uint8_t pin)
   return ADC;
 }
 
+//Stands for toggle Digital Pin, if the pin (pinnumbers are based on the arduino uno board) is a logical LOW then it returns 0 if it's HIGH then the function returns 1
 void Atmega328P::toggleDP(uint8_t pin)
 {
   uint8_t value = getPinStat(pin);
   DW(pin,!value);
 }
 
+//When a digital pin is set to output this function can get wheter the pin is set to a logical LOW or HIGH
 uint8_t Atmega328P::getPinStat(uint8_t pin)
 {
   uint8_t bit=0B0;
@@ -124,30 +226,22 @@ uint8_t Atmega328P::getPinStat(uint8_t pin)
   return bit;
 }
 
+//Function to display a byte on 8 leds in binary, PORTB is used for this.
 void Atmega328P::binToLed(uint8_t byte)
 {
     int index = 7;
     int pin = 13;
     while(index >= 0)
     {
-      // Serial.print(getBit(byte, pin-(pin-7)));
       DW(pin,getBit(byte, index));
       index--;
       pin--;
     }
-    // Serial.println();
 }
 
-uint8_t Atmega328P::getBit(uint8_t data, uint8_t index)
-{
-  //data HEX number from which the bit is asked
-  //index is from lsb to msb ie. 0100 is 2 (3210)
-  uint8_t bit = 0B0;
-  data &= 1 << index;
-  bit = data >> index;
-  return bit;
-}
+//I2C
 
+//Function to send a start signal on the I2C bus
 void Atmega328P::sendI2CStart()
 {
   TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
@@ -155,6 +249,7 @@ void Atmega328P::sendI2CStart()
   if(DEBUG){Serial.println(TWSR,HEX);};
 }
 
+//Function to send a repeated start signal on the I2C bus
 void Atmega328P::sendI2CReStart()
 {
   TWCR = (1<<TWSTA)|(1<<TWINT);
@@ -163,13 +258,14 @@ void Atmega328P::sendI2CReStart()
   if(DEBUG){Serial.println(TWSR,HEX);};
 }
 
+//Function to send a stop signal on the I2C bus
 void Atmega328P::sendI2CStop()
 {
   TWCR = (1<<TWINT) |(1<<TWSTA);
   _delay_ms(10);
-  // if(DEBUG){Serial.println(TWSR,HEX);};
 }
 
+//Function to send a slave address on the I2C bus
 void Atmega328P::sendI2CAddr(uint8_t addr)
 {
   TWDR = addr;
@@ -178,6 +274,7 @@ void Atmega328P::sendI2CAddr(uint8_t addr)
   if(DEBUG){Serial.println(TWSR,HEX);};
 }
 
+//Function to send data on the I2C bus
 void Atmega328P::sendI2CData(uint8_t data)
 {
   TWDR = data;
@@ -186,6 +283,7 @@ void Atmega328P::sendI2CData(uint8_t data)
   if(DEBUG){Serial.println(TWSR,HEX);};
 }
 
+//Function to read a byte from the the I2C bus
 uint8_t Atmega328P::readI2CData()
 {
   while (!(TWCR &  (1<<TWINT)));
@@ -193,93 +291,8 @@ uint8_t Atmega328P::readI2CData()
   return data;
 }
 
-void Atmega328P::UARTInit(uint8_t dataBits, uint8_t parityBit, uint8_t stopBits, float baud, uint8_t speed, uint8_t RT)
-{
-  uint16_t ubrr = round(((clockspeed / ((speed + 1) * 8 * baud)) - 1));
-  // binToLed(ubrr);
-  UCSR0A &= ~(1 << U2X0);
-  UBRR0H = (unsigned char)(ubrr>>8);
-  UBRR0L = (unsigned char)ubrr;
-  // Enable receiver and transmitter
-  UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<UCSZ02);
-  /* Set frame format: 8data, 1 stop bit */
-  UCSR0C = (3<<UCSZ00);
-}
 
-void Atmega328P::UARTBegin(long int baud)//function to begin the UART with the defaul settings and a baudrate. returns an status code.
-{
-  UARTInit(0, 0, 0, baud, NORMALSPEED, 0);
-}
-
-void Atmega328P::UARTSend(uint16_t data)
-{
-  // /* Wait for empty transmit buffer */
-  // while ( !( UCSR0A & (1<<UDRE0)) );
-  // /* Put data into buffer, sends the data */
-  // UDR0 = data;
-}
-
-void Atmega328P::UARTSend(uint8_t data)
-{
-  /* Wait for empty transmit buffer */
-  while ( !( UCSR0A & (1<<UDRE0)) );
-  /* Put data into buffer, sends the data */
-  UDR0 = data;
- }
-
-void Atmega328P::UARTSendBytes(uint8_t bytes[], uint8_t amountOfBytes)
-{
-  int index = 0;
-  while(index < amountOfBytes - 1)
-  {
-    UARTSend(bytes[index]);
-    index++;
-  }
-}
-uint8_t Atmega328P::UARTRead()
-{
-  int counter = 0;
-  while ((!(UCSR0A & _BV(RXC0))) && counter < 75)
-  {
-    _delay_ms(1);
-    counter++;
-  }
-  return (uint8_t) UDR0;
-}
-
-void Atmega328P::UARTREADBytes(uint8_t *bytes, uint8_t amountOfBytes)
-{
-  int index = 0;
-  uint8_t* temp = bytes;
-  temp = bytes + (sizeof(uint8_t));
-    while(index <= amountOfBytes)
-    {
-      // UARTSend((uint8_t) (0x30 + index));
-      temp = bytes + (sizeof(uint8_t) * index);
-      *temp =  UARTRead();
-      // binToLed(*temp);
-      index++;
-    }
-    // binToLed((uint8_t) 0xff);
-}
-
-void Atmega328P::print(uint8_t *bytes)
-{
-  int index = 0;
-  while (bytes[index] != '\0')
-  {
-    index++;
-  }
-  UARTSendBytes(bytes, index + 1 );
-}
-
-void Atmega328P::println(uint8_t *bytes)
-{
-  print(bytes);
-  UARTSend((uint8_t) 0x0A);
-  UARTSend((uint8_t) 0x0D);
-}
-
+//Function to write to the EEPROM. addr is the address to wich the data is written
 void Atmega328P::EEPROM_Write(uint32_t addr, uint8_t data)
 {
   /* Wait for completion of previous write */
@@ -293,6 +306,7 @@ void Atmega328P::EEPROM_Write(uint32_t addr, uint8_t data)
   EECR |= (1<<EEPE);
 }
 
+//Function to write multiple bytes to EEPROM. The function takes an address (uint32_t addr) of the first memory address, an array with data bytes and the amount of bytes.
 void Atmega328P::EEPROM_WriteBytes(uint32_t addr, uint8_t data[], uint32_t amountOfBytes)
 {
   for (size_t i = 0; i < amountOfBytes; i++)
@@ -301,6 +315,7 @@ void Atmega328P::EEPROM_WriteBytes(uint32_t addr, uint8_t data[], uint32_t amoun
   }
 }
 
+//Function to read from the EEPROM. addr is the address from wich the data is read
 uint8_t Atmega328P::EEPROM_Read(uint32_t addr)
 {
   /* Wait for completion of previous write */
@@ -313,10 +328,24 @@ uint8_t Atmega328P::EEPROM_Read(uint32_t addr)
   return EEDR;
 }
 
+//Function to read multiple bytes from the EEPROM. The function takes
 void Atmega328P::EEPROM_ReadBytes(uint32_t addr, uint8_t data[], uint32_t amountOfBytes)
 {
   for (size_t i = 0; i < amountOfBytes; i++)
   {
     data[i] = EEPROM_Read(addr + i);
   }
+}
+
+//MISC
+
+//gets the bit for the data (a byte) at the index
+uint8_t Atmega328P::getBit(uint8_t data, uint8_t index)
+{
+  //data HEX number from which the bit is asked
+  //index is from lsb to msb ie. 0100 is 2 (3210)
+  uint8_t bit = 0B0;
+  data &= 1 << index;
+  bit = data >> index;
+  return bit;
 }
--- Atmega328P.h
+++ Atmega328P.h
@@ -1,10 +1,7 @@
 #ifndef Atmega328P_H
 #define Atmega328P_H
 //made by Rick Overhorst
-//basic funcitions for the Arduino UNO
-
-
-
+//an implementation of the Microcontroller interface
 //includes
 #include <stdint.h>
 #include <avr/io.h>
@@ -21,37 +18,45 @@ class Atmega328P : public Microcontroller
     private:
 
     public:
-       void    UARTInit(uint8_t dataBits, uint8_t parityBit, uint8_t stopBits, float baud, uint8_t speed, uint8_t RT); //fuction to initialize the UART, takes the amount of data bits (5,6,6,8 or 9 bits), no, even or an odd parity bit, the amound of stop bits (1 or 2), baudrate and whether or not receive and transmit should be enabled
-       void    UARTBegin(long int baud); //function to begin the UART with the defaul settings and a baudrate. returns an status code.
-       void    UARTSend(uint16_t data);
-       void    UARTSend(uint8_t data);
-       void    UARTSendBytes(uint8_t bytes[], uint8_t amountOfBytes);
-       uint8_t UARTRead();
-       void    UARTREADBytes(uint8_t *bytes, uint8_t amountOfBytes);
-       void    println(uint8_t *bytes);
-       void    print(uint8_t *bytes);
-       void    setDPM(uint8_t pin, uint8_t value);// set pin mode value = 0 or 1 for the digital pins
-       void    setAPM(uint8_t pin, uint8_t value);// set pin mode value = 0 or 1 for the analog pins
-       void    DW(uint8_t pin, uint8_t value);//digital write(dw) value = 0 or 1
-       uint8_t DR(uint8_t pin);//digital read
-       void    AW(uint8_t pin, uint8_t value);//same as dw but with analog pin
-       uint16_t AR(uint8_t pin);
-       void    toggleDP(uint8_t pin);
-       uint8_t getPinStat(uint8_t pin);
-       void    binToLed(uint8_t byte);
+       //UART
+       void    UARTInit(uint8_t dataBits, uint8_t parityBit, uint8_t stopBits, float baud, uint8_t speed, uint8_t RT);  //Function to initialize the UART, takes the amount of data bits (5,6,6,8 or 9 bits), no, even or an odd parity bit, the amound of stop bits (1 or 2), baudrate and whether or not receive and transmit should be enabled
+       void    UARTBegin(long int baud);                                                                                //Function to begin the UART with the defaul settings and a baudrate.
+       void    UARTSend(uint16_t data);                                                                                 //Function to send 9 bits, warning: not yet implemented
+       void    UARTSend(uint8_t data);                                                                                  //function to send less 9 bits
+       void    UARTSendBytes(uint8_t bytes[], uint8_t amountOfBytes);                                                   //Function to send multiple bytes. Takes an Array of bytes to send and the amount of bytes to send
+       uint8_t UARTRead();                                                                                              //Function to read a byte
+       void    UARTREADBytes(uint8_t *bytes, uint8_t amountOfBytes);                                                    //Function to read multiple bytes. Takes an Array of bytes/pointer to put the bytes in when they are read and the amount of bytes to read
+       void    println(uint8_t *bytes);                                                                                 //Function to sends a char array/string and sets  a new line
+       void    print(uint8_t *bytes);                                                                                   //Function to sends a char array/string and doesn't set a new line
 
-       uint8_t getBit(uint8_t data, uint8_t index);//gets the bit for the data (a byte) at the index
-       void    sendI2CStart();
-       void    sendI2CReStart();
-       void    sendI2CStop();
-       void    sendI2CAddr(uint8_t addr);
-       void    sendI2CData(uint8_t data);
-       uint8_t readI2CData();
+       //GPIO
+       void    setDPM(uint8_t pin, uint8_t value);  //Stands for set Digital Pin Mode. Sets the data directions of a digital pin (pinnumbers are based on the arduino uno board) value = 0 or 1 for input or output respectively
+       void    setAPM(uint8_t pin, uint8_t value);  //Stands for set AnaLog Pin Mode. Sets the data directions of a analog pin (pinnumbers are based on the arduino uno board) value = 0 or 1 for input or output respectively
+       void    DW(uint8_t pin, uint8_t value);      //Stands for Digital Write(dw). Sets the pin (based on the pinnumbers on the arduino uno board) to a logical LOW or HIGH based on value which can be 0 or 1.
+       uint8_t DR(uint8_t pin);                     //Stands for Digital Read, if the pin has been set to input this function can read if it's set to a logical LOW or HIGH
+       void    AW(uint8_t pin, uint8_t value);      //This function is not PWM, this function mimics the DW (Digital Write) but with the analog pins
+       uint16_t AR(uint8_t pin);                    //Stands for Analog Read. Function to the read an analog value value on the pin (based on the Arduino UNO pinnumbers)
+       void    toggleDP(uint8_t pin);               //Stands for toggle Digital Pin, if the pin (pinnumbers are based on the arduino uno board) is a logical LOW then it returns 0 if it's HIGH then the function returns 1
+       uint8_t getPinStat(uint8_t pin);             //When a digital pin is set to output this function can get wheter the pin is set to a logical LOW or HIGH
+       void    binToLed(uint8_t byte);              //Function to display a byte on 8 leds in binary, PORTB is used for this.
 
-       void    EEPROM_Write(uint32_t addr, uint8_t data);
-       void    EEPROM_WriteBytes(uint32_t addr, uint8_t data[], uint32_t amountOfBytes);
-       uint8_t EEPROM_Read(uint32_t addr);
-       void    EEPROM_ReadBytes(uint32_t addr, uint8_t data[], uint32_t amountOfBytes);
+       //I2C
+       void    sendI2CStart();            //Function to send a start signal on the I2C bus
+       void    sendI2CReStart();          //Function to send a repeated start signal on the I2C bus
+       void    sendI2CStop();             //Function to send a stop signal on the I2C bus
+       void    sendI2CAddr(uint8_t addr); //Function to send a slave address on the I2C bus
+       void    sendI2CData(uint8_t data); //Function to send data on the I2C bus
+       uint8_t readI2CData();             //Function to read a byte from the the I2C bus
 
+       //EEPROM
+       void    EEPROM_Write(uint32_t addr, uint8_t data);                                 //Function to write to the EEPROM. addr is the address to wich the data is written
+       void    EEPROM_WriteBytes(uint32_t addr, uint8_t data[], uint32_t amountOfBytes);  //Function to write multiple bytes to EEPROM. The function takes an address (uint32_t addr) of the first memory address, an array with data bytes and the amount of bytes.
+       uint8_t EEPROM_Read(uint32_t addr);                                                //Function to read from the EEPROM. addr is the address from wich the data is read
+       void    EEPROM_ReadBytes(uint32_t addr, uint8_t data[], uint32_t amountOfBytes);   //Function to read multiple bytes from the EEPROM. The function takes
+
+
+       //misc
+       //gets the bit for the data (a byte) at the index
+       uint8_t getBit(uint8_t data, uint8_t index);
   };
 #endif
